0ac0eb48f6f8309d43a07e960f01b333f36cc8a9
kris.foster <me@krismade.me>
2022-05-17 18:20:17 +0000

    AUtomated stress testing for the jibber app

--- benchmarks/benchmark-jibber/deploy.sh
+++ benchmarks/benchmark-jibber/deploy.sh
@@ -8,3 +8,6 @@ kubectl create -f k8s/jibber-openjdk.yaml
 kubectl create -f k8s/jibber-native.yaml
 #kubectl create -f k8s/jibber-graalee.yaml
 #kubectl create -f k8s/jibber-native-opts.yaml
+
+# Deploy the stress testing container
+kubectl create -f k8s/stress-test-job.yaml
