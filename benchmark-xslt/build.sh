af34d004d74c62c38fa19777464655092cbefb46
kris.foster <me@krismade.me>
2022-05-30 10:49:04 +0000

    Added Support for the XSLT Bencmark / Workload

--- benchmark-xslt/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Build the openJDK container and push it out
-mvn clean package
-docker push phx.ocir.io/oracledeveloper/k8s-perf-demo:transform.openjdk.0.0.2-SNAPSHOT
-
-# Build the Native Image Container and push it out
-mvn clean package -Ddocker-file=Dockerfiles/Dockerfile.native -Dbase-image-tag=native -Pnative
-docker push phx.ocir.io/oracledeveloper/k8s-perf-demo:transform.native.0.0.2-SNAPSHOT
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,22 +19,43 @@ kubectl apply -f k8s/grafana-deployment.yaml
 # Build the benchmark benchmarks and push them to the container reg
 mvn clean package -Ddocker-repository=${REPO_PATH}
 docker push ${REPO_PATH}:jibber.openjdk.latest
+docker push ${REPO_PATH}:transform.openjdk.latest
 
 # Build the Native Image Container benchmark and push them to the container reg
 mvn clean package -Ddocker-repository=${REPO_PATH} -Ddocker-file=Dockerfiles/Dockerfile.native -Dbase-image-tag=native -Pnative
 docker push ${REPO_PATH}:jibber.native.latest
+docker push ${REPO_PATH}:transform.native.latest
 
 # Build the GraalVM EE JIT Image and push them to the container reg
 mvn clean package -Ddocker-repository=${REPO_PATH} -Ddocker-file=Dockerfiles/Dockerfile.graalee -Dbase-image-tag=graalee
 docker push ${REPO_PATH}:jibber.graalee.latest
+docker push ${REPO_PATH}:transform.graalee.latest
 
-# Deploy the Jibber benchmark
-kubectl create -f benchmark-jibber/k8s/namespace.yaml
+#############################
+# Jibber Benchmark
+#############################
+
+# Deploy the namespace
+kubectl apply -f benchmark-jibber/k8s/namespace.yaml
 
 # Deploy the containers to k8s
-cat benchmark-jibber/k8s/jibber-openjdk.yaml | envsubst | kubectl create -f -
-cat benchmark-jibber/k8s/jibber-native.yaml  | envsubst | kubectl create -f -
-cat benchmark-jibber/k8s/jibber-graalee.yaml | envsubst | kubectl create -f -
+cat benchmark-jibber/k8s/jibber-openjdk.yaml | envsubst | kubectl apply -f -
+cat benchmark-jibber/k8s/jibber-native.yaml  | envsubst | kubectl apply -f -
+cat benchmark-jibber/k8s/jibber-graalee.yaml | envsubst | kubectl apply -f -
 
 # Deploy the stress testing container
-kubectl create -f benchmark-jibber/k8s/stress-test-job.yaml
+kubectl apply -f benchmark-jibber/k8s/stress-test-job.yaml
+
+#############################
+# XSLT Benchmark
+#############################
+# Deploy the namespace
+kubectl apply -f benchmark-xslt/k8s/namespace.yaml
+
+# Deploy the containers to k8s
+cat benchmark-xslt/k8s/transform-openjdk.yaml | envsubst | kubectl apply -f -
+cat benchmark-xslt/k8s/transform-native.yaml  | envsubst | kubectl apply -f -
+cat benchmark-xslt/k8s/transform-graalee.yaml | envsubst | kubectl apply -f -
+
+# Deploy the stress testing container
+kubectl apply -f benchmark-xslt/k8s/stress-test-job.yaml
