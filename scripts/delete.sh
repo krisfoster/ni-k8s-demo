3e821bbfb3622c1e6d9cb6efe0fde510c0c6fd4c
Sachin Pikle <spikle.dev@gmail.com>
2022-06-06 19:56:40 +0530

    Update delete.sh

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kubectl delete  -f k8s/namespace.yaml
+kubectl delete -f k8s/namespace.yaml
 kubectl delete -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/aio/deploy/recommended.yaml
 kubectl delete -f k8s/dashboard-admin.yaml
 kubectl delete -f k8s/clusterRole.yaml
@@ -10,4 +10,8 @@ kubectl delete -f k8s/prom-deployment.yaml
 kubectl delete -f k8s/grafana-config-map.yaml
 kubectl delete -f k8s/grafana-deployment.yaml
 
-
+## Deleting a namespace automatically deletes all the K8s resources in that namespace
+### Jibber
+kubectl delete -f benchmark-jibber/k8s/namespace.yaml
+### Transform
+kubectl delete -f benchmark-xslt/k8s/namespace.yaml
