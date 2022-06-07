3b5ed64490d073e4929ac6baeba79bdd0f3bc684
Sachin Pikle <spikle.dev@gmail.com>
2022-06-07 12:01:43 +0530

    Comment kube apply graalee & docker push transform

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,12 +19,12 @@ kubectl apply -f k8s/grafana-deployment.yaml
 # Build the benchmark benchmarks and push them to the container reg
 mvn clean package -Ddocker-repository=${REPO_PATH}
 docker push ${REPO_PATH}:jibber.openjdk.latest
-docker push ${REPO_PATH}:transform.openjdk.latest
+# docker push ${REPO_PATH}:transform.openjdk.latest
 
 # Build the Native Image Container benchmark and push them to the container reg
 mvn package -Ddocker-repository=${REPO_PATH} -Ddocker-file=Dockerfiles/Dockerfile.native -Dbase-image-tag=native -Pnative
 docker push ${REPO_PATH}:jibber.native.latest
-docker push ${REPO_PATH}:transform.native.latest
+# docker push ${REPO_PATH}:transform.native.latest
 
 # Build the GraalVM EE JIT Image and push them to the container reg
 #mvn package -Ddocker-repository=${REPO_PATH} -Ddocker-file=Dockerfiles/Dockerfile.graalee -Dbase-image-tag=graalee
@@ -41,7 +41,7 @@ kubectl apply -f benchmark-jibber/k8s/namespace.yaml
 # Deploy the containers to k8s
 cat benchmark-jibber/k8s/jibber-openjdk.yaml | envsubst | kubectl apply -f -
 cat benchmark-jibber/k8s/jibber-native.yaml  | envsubst | kubectl apply -f -
-cat benchmark-jibber/k8s/jibber-graalee.yaml | envsubst | kubectl apply -f -
+# cat benchmark-jibber/k8s/jibber-graalee.yaml | envsubst | kubectl apply -f -
 
 # Deploy the stress testing container
 kubectl apply -f benchmark-jibber/k8s/stress-test-job.yaml
@@ -55,7 +55,7 @@ kubectl apply -f benchmark-xslt/k8s/namespace.yaml
 # Deploy the containers to k8s
 cat benchmark-xslt/k8s/transform-openjdk.yaml | envsubst | kubectl apply -f -
 cat benchmark-xslt/k8s/transform-native.yaml  | envsubst | kubectl apply -f -
-cat benchmark-xslt/k8s/transform-graalee.yaml | envsubst | kubectl apply -f -
+# cat benchmark-xslt/k8s/transform-graalee.yaml | envsubst | kubectl apply -f -
 
 # Deploy the stress testing container
 kubectl apply -f benchmark-xslt/k8s/stress-test-job.yaml
